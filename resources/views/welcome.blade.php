80940782e3b7ae3ab2c46e1c463ca0cef242e1b4
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-12-04 10:24:45 +0800

    kemaskan ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,27 +2,31 @@
 <html lang="en">
 
 <head>
-  <title>Bootstrap Example</title>
+  <title>Meme generator</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 
 <body>
 
   <div class="container">
-    <h2>Meme Generator</h2>
-    <p>Press generate bbutton to generate list of memes</p>
-    <a href="/generate-meme" class="btn btn-success mb-4">Generate Meme</a>
-    <table class="table">
+    <div class="text-center mb-4">
+      <h2>Meme Generator</h2>
+      <p>Press generate button to generate list of memes</p>
+      <a href="/generate-meme" class="btn btn-success">Generate Meme</a>
+    </div>
+    <table class="table table-hover">
       <thead>
         <tr>
           <th>#</th>
           <th>Meme</th>
-          <th>Image</th>
+          <th class="text-center">Image</th>
         </tr>
       </thead>
       <tbody>
@@ -30,7 +34,7 @@
         <tr>
           <td>{{$loop->iteration}}</td>
           <td>{{$meme->name}}</td>
-          <td><img src="{{$meme->url}}" alt="img" width=10%></td>
+          <td class="text-center"><img src="{{$meme->url}}" alt="img" width=20%></td>
         </tr>
         @endforeach
       </tbody>
@@ -38,5 +42,9 @@
   </div>
 
 </body>
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
 </html>
